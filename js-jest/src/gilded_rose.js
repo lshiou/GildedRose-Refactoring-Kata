d92865cbdf353d497e22587416a63a442a4db1b0
Lawrence Shiou <lawrence.shiou@gmail.com>
2025-03-12 17:04:10 -0700

    clean up logic for default items

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -35,15 +35,15 @@ class Shop {
 
         item.sellIn -= 1;
       } else {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1;
+        if (item.sellIn > 0) {
+          item.quality -= 1;
+        } else {
+          item.quality -= 2;
         }
-        item.sellIn = item.sellIn - 1;
+        item.sellIn -= 1;
 
-        if (item.sellIn < 0) {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1;
-          }
+        if (item.quality < 0) {
+          item.quality = 0;
         }
       }
     }
