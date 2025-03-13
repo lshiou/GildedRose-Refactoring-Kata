5504ec0500b0a5d96453fa46619ceb639beeb296
Lawrence Shiou <lawrence.shiou@gmail.com>
2025-03-13 11:02:11 -0700

    extract logic into a handler object with corresponding functions for the various item types

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -6,46 +6,64 @@ class Item {
   }
 }
 
+const ItemHandler = {
+  "Aged Brie": (item) => {
+    item.sellIn -= 1;
+    if (item.quality < 50) {
+      item.quality += 1;
+    }
+  },
+  "Sulfuras, Hand of Ragnaros": (item) => {
+    return;
+  },
+  "Backstage passes to a TAFKAL80ETC concert": (item) => {
+    if (item.quality != 50) {
+      if (item.sellIn > 10) {
+        item.quality += 1;
+      } else if (item.sellIn > 5) {
+        item.quality += 2;
+      } else if (item.sellIn > 0) {
+        item.quality += 3;
+      } else {
+        item.quality = 0;
+      }
+    }
+
+    item.sellIn -= 1;
+  },
+  default: (item) => {
+    if (item.sellIn > 0) {
+      item.quality -= 1;
+    } else {
+      item.quality -= 2;
+    }
+    item.sellIn -= 1;
+
+    if (item.quality < 0) {
+      item.quality = 0;
+    }
+  },
+};
+
+const SpecialtyItemName = [
+  "Aged Brie",
+  "Sulfuras, Hand of Ragnaros",
+  "Backstage passes to a TAFKAL80ETC concert",
+];
+
 class Shop {
   constructor(items = []) {
     this.items = items;
   }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
-      if (item.name == "Aged Brie") {
-        item.sellIn -= 1;
-        if (item.quality < 50) {
-          item.quality += 1;
-        }
-      } else if (item.name == "Sulfuras, Hand of Ragnaros") {
-        continue;
-      } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.quality != 50) {
-          if (item.sellIn <= 0) {
-            item.quality = 0;
-          } else if (item.sellIn <= 5) {
-            item.quality += 3;
-          } else if (item.sellIn <= 10) {
-            item.quality += 2;
-          } else {
-            item.quality += 1;
-          }
-        }
+      const handlerName = SpecialtyItemName.includes(item.name)
+        ? item.name
+        : "default";
 
-        item.sellIn -= 1;
-      } else {
-        if (item.sellIn > 0) {
-          item.quality -= 1;
-        } else {
-          item.quality -= 2;
-        }
-        item.sellIn -= 1;
-
-        if (item.quality < 0) {
-          item.quality = 0;
-        }
-      }
+      ItemHandler[handlerName](item);
     }
 
     return this.items;
